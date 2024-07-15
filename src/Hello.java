260809197482d49383a18ed20af204cd4eb02a90
zyalin459 <zyalin459@gmail.com>
2024-07-15 02:23:33 -0700

    WEEK 1

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+// ******** WEEK 1 *************
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("echo: " + in.nextLine());
+        System.out.println(2+3+"=2+3="+(2+3));
+    }
+}
